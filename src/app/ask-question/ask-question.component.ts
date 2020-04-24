1ccd9b5c850fcbf1f9d3ba27c9b07e105bcc40bf
Shristi Katyayani <shristik@vmware.com>
2020-04-24 14:23:35 +0530

    submit questions

--- src/app/ask-question/ask-question.component.ts
+++ src/app/ask-question/ask-question.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl} from '@angular/forms';
+import { AskForm } from '../askForm';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ask-question',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ask-question.component.css']
 })
 export class AskQuestionComponent implements OnInit {
+  url: string =  'http://10.89.241.17/Server/api/question';
+  questionid: number;
+  constructor(private httpClient: HttpClient, private router: Router) { }
+    askForm = new FormGroup({
+    QuestionTitle: new FormControl('', Validators.required),
+    QuestionDescription: new FormControl(''),
+      });
+    askFormObj = new AskForm(undefined, undefined, 1);
+    ngOnInit(): void {
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
+  submitQuestion() {
+    console.log(this.askFormObj);
+    this.httpClient.post(this.url, this.askFormObj, {headers : new HttpHeaders({'content-type': 'application/json'})}
 
+    )
+      .subscribe(data => {
+          console.log(data);
+      });
+  }
 }
